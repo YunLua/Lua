b861d413c9c21da913ed35e6011d668c07658e1a
YunLua <3838894873@qq.com>
2025-11-29 03:39:04 +0800

    Update 选择服务器.lua

--- 选择服务器.lua
+++ 选择服务器.lua
@@ -1,5 +1,6 @@
 return {
     ["极速传奇"] = "https://github.com/YunLua/Lua/raw/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.lua",
+    ["在披萨店工作"] = "https://github.com/YunLua/Lua/raw/main/在披萨店工作.lua",
     ["在森林中的99夜"] = "https://github.com/YunLua/Lua/raw/main/99.lua",
     ["忍者传奇"] = "https://github.com/YunLua/Lua/raw/main/忍者.lua",
     ["吃掉世界"] = "https://github.com/YunLua/Lua/raw/main/吃掉世界.lua",
