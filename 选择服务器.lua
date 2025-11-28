c9c880e2b51d62c179a1007d9249e0fbfaf0a73c
YunLua <3838894873@qq.com>
2025-11-29 05:01:18 +0800

    Update 选择服务器.lua

--- 选择服务器.lua
+++ 选择服务器.lua
@@ -5,6 +5,7 @@ return {
     ["忍者传奇"] = "https://github.com/YunLua/Lua/raw/main/忍者.lua",
     ["吃掉世界"] = "https://github.com/YunLua/Lua/raw/main/吃掉世界.lua",
     ["火箭模拟器"] = "https://github.com/YunLua/Lua/raw/main/火箭模拟器.lua",
+    ["自然灾害生存游戏"] = "https://github.com/YunLua/Lua/raw/main/自然灾害生存游戏.lua",
     ["虫脸"] = "https://github.com/YunLua/Lua/raw/main/虫脸.lua",
     ["动漫明星模拟器"] = "https://github.com/YunLua/Lua/raw/main/动漫模拟器.lua"
 }
