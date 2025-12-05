cc8ba1d1d90f23dd91360c8bd3bc17a859d3e2d8
小云呀 <3838894873@qq.com>
2025-12-05 17:27:34 +0800

    Update 选择服务器.lua

--- 选择服务器.lua
+++ 选择服务器.lua
@@ -2,7 +2,7 @@ return {
     ["极速传奇"] = "https://github.com/YunLua/Lua/raw/main/%E6%9E%81%E9%80%9F%E4%BC%A0%E5%A5%87.lua",
     ["在披萨店工作"] = "https://github.com/YunLua/Lua/raw/main/在披萨店工作.lua",
     ["在森林中的99夜"] = "https://github.com/YunLua/Lua/raw/main/99.lua",
-    ["忍者传奇"] = "https://github.com/YunLua/Lua/raw/main/忍者.lua",
+    ["忍者传奇"] = "https://github.com/YunLua/Lua/raw/main/忍者传奇.lua",
     ["吃掉世界"] = "https://github.com/YunLua/Lua/raw/main/吃掉世界.lua",
     ["火箭模拟器"] = "https://github.com/YunLua/Lua/raw/main/火箭模拟器.lua",
     ["自然灾害生存游戏"] = "https://github.com/YunLua/Lua/raw/main/自然灾害生存游戏.lua",
