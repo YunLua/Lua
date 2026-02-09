14c34c6d1164435deac84ae8eaa1f33870cb5a16
XiaoYunUwU <id_09128616@foxmail.com>
2026-02-09 14:46:22 +0800

    更新 English.lua

--- English.lua
+++ English.lua
@@ -1,1 +1,47 @@
-loadstring(game:HttpGet("https://gitee.com/XiaoYunCN/lol/raw/master/云脚本.lua", true))()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "云脚本",
+    Size = UDim2.fromOffset(460, 310),
+    TabWidth = 160,
+    Theme = "Darker"
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "请选择UI", Icon = "box" })
+}
+
+Tabs.Main:AddParagraph({
+    Title = "反馈",
+    Content = "请加入新云脚本群号1083844758"
+})
+
+Tabs.Main:AddButton({
+    Title = "加载LuaWareUI[silentUI]版本",
+    Description = "UI简洁经典，加载快速\n目前为最新版本",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/YunLua/Lua/main/silentUI.luau"))()
+    end
+})
+Tabs.Main:AddButton({
+    Title = "复制LuaWareUI版本",
+    Callback = function()
+        setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/YunLua/Lua/main/silentUI.luau"))()')
+    end
+})
+
+Tabs.Main:AddButton({
+    Title = "加载windUI版本",
+    Description = "UI好看实用，加载较慢\n目前为旧版本，可能不会再更新",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/YunLua/Lua/main/WindUI.luau",true))()
+    end
+})
+Tabs.Main:AddButton({
+    Title = "复制windUI版本",
+    Callback = function()
+        setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/YunLua/Lua/main/WindUI.luau",true))()')
+    end
+})
+
+Window:SelectTab(1)
